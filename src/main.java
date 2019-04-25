a9a4699669e7360986afcef5089db01267df4c05
Elana Olson <elanaolson@gmail.com>
2019-04-25 15:06:40 -0700

    select a month to display base methods

--- src/main.java
+++ src/main.java
@@ -4,7 +4,26 @@ public class main {
 	public static void main(String[] args) {
 		final DateSet dateset = new DateSet(); //Model
 		Calendar calendar = new Calendar(); //View
-	
+		
+		/*
+		 * MVC: Model-View-Controller interactions
+		 * 
+		 * If user clicks on a date on the calendar
+		 * -> update selectedEntry in DateSet (M)
+		 * 
+		 * If selectedEntry is changed
+		 * -> change displayed ToDoList (V)
+		 * 
+		 * -----
+		 * 
+		 * If user clicks on the month on the calendar
+		 * -> update selectedMonth in DateSet (M)
+		 * 
+		 * If selectedMonth is changed
+		 * -> change displayed ToDoList to combine all 
+		 * ListItems from all ToDoLists in that month (V)
+		 * 
+		 */
 		
 		
 	}
